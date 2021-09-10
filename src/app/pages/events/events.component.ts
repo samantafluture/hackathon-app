fe4010d85de16d43f72d0ffa9519d3242118dbba
Samanta Fluture <samantafluture@gmail.com>
2021-09-09 21:18:57 -0300

    use events as observable on template component

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { Events } from 'src/app/interfaces/event';
 import { EventService } from 'src/app/services/event.service';
 
 @Component({
@@ -9,19 +7,9 @@ import { EventService } from 'src/app/services/event.service';
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css'],
 })
-export class EventsComponent implements OnInit, OnDestroy {
+export class EventsComponent {
   eventsInput = new FormControl();
-  events!: Events;
-  private subscription!: Subscription;
+  events$ = this.eventService.getAllEvents();
 
   constructor(private eventService: EventService) {}
-  ngOnInit(): void {
-    this.subscription = this.eventService.getAllEvents().subscribe((events) => {
-      this.events = events;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
